9d7f7f7d13a4d3661ec9b805e16849fff987f675
Volodymir <151960820+vovan0977967895@users.noreply.github.com>
2024-01-11 21:22:54 +0200

    48-00

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@ const game = document.getElementById('game')
 
 
 let cardCount = 4;
-
 const cardNumberArray = [];
+let firstCard = null;
+let secondCard = null;
 
 
 //Создание массива чисел
@@ -25,9 +26,53 @@ for(let i = 0; i < cardNumberArray.length; i++){
 
 for(const cardNumber of cardNumberArray){
     let card = document.createElement('div');
+    console.log(card);
     card.textContent = cardNumber;
+    card.classList.add('card')
 
-    game.append(card);
+    // Клик по каточке
+    card.addEventListener('click', function(){
+        if(card.classList.contains('open') || card.classList.contains('success')){
+            return
+        };
+
+
+        if(firstCard !== null && secondCard !== null){
+            firstCard.classList.remove('open');
+            secondCard.classList.remove('open');
+            firstCard = null;
+            secondCard = null;
+        }
+       card.classList.add('open') 
+       console.log('Карточка по которой произошел клик', card);
+
+       if(firstCard === null){
+        firstCard = card;
+       }else{
+        secondCard = card;
+       }
+       if(firstCard !== null && secondCard !== null){
+        let firstCardNumber = firstCard.textContent;
+        let secondCardNumber = secondCard.textContent;
+        if(firstCardNumber === secondCardNumber){
+            firstCard.classList.add('success');
+            secondCard.classList.add('success');
+            console.log('Обе карточки совпали');
+        }
+        console.log('Обе карточки відкриті');
+
+       }
+       if(cardNumberArray.length === document.querySelectorAll('.success').length){
+        setTimeout(function() {
+            alert('Victory!!!')
+        }, 400)
+       }
+       console.log('firstCard',firstCard);
+       console.log('secondCard',secondCard);
+    })
+    
+
+   game.append(card);
 }
 
-console.log(cardNumberArray)
+console.log(cardNumberArray);
